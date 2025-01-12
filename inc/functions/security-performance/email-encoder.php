be12ac4ed7a6d1b44e33eee5a8e239f28ced57c8
Jacek Nicpoń <fulcro@fulcro.pl>
2025-01-12 01:34:34 +0100

    Email Encoder Mod

--- inc/functions/security-performance/email-encoder.php
+++ inc/functions/security-performance/email-encoder.php
@@ -7,10 +7,32 @@ if ( ! defined( 'DEE_FILTER_PRIORITY' ) ) {
 	define( 'DEE_FILTER_PRIORITY', 1000 );
 }
 
+// Define regular expression 
+if ( ! defined( 'DEE_REGEXP' ) ) {
+    define(
+        'DEE_REGEXP',
+        '{
+            (?:mailto:)?
+            (?:
+                [-!#$%&*+/=?^_`.{|}~\w\x80-\xFF]+
+            |
+                ".*?"
+            )
+            \@
+            (?:
+                [-a-z0-9\x80-\xFF]+(\.[-a-z0-9\x80-\xFF]+)*\.[a-z]+
+            |
+                \[[\d.a-fA-F:]+\]
+            )
+        }xi'
+    );
+}
+
 // Register filters to encode email addresses in various content areas
 foreach ( array( 'the_content', 'the_excerpt', 'widget_text', 'comment_text', 'comment_excerpt' ) as $filter ) {
 	add_filter( $filter, 'devops_ee_encode_emails', DEE_FILTER_PRIORITY );
 }
+add_filter( 'walker_nav_menu_start_el', 'devops_ee_encode_emails', DEE_FILTER_PRIORITY );
 
 // Check if shortcocde option is enabled
 function devops_ee_shortcode_is_enabled() {
@@ -34,8 +56,44 @@ function devops_ee_register_shortcode() {
 }
 
 // Shortcode callback to encode text
-function devops_ee_shortcode( $attributes, $content = '' ) {
+/*function devops_ee_shortcode( $attributes, $content = '' ) {
     return devops_ee_encode_str( $content );
+}*/
+
+// Shortcode callback to encode text
+function devops_ee_shortcode( $attributes, $content = '' ) {
+    $atts = shortcode_atts( array(
+        'link' => null,
+        'class' => null,
+    ), $attributes, 'encode' );
+
+    // override encoding function with the 'devops_ee_method' filter
+    $method = apply_filters( 'devops_ee_method', 'devops_ee_encode_str' );
+
+    if ( ! empty( $atts[ 'link' ] ) ) {
+        $link = esc_url( $atts[ 'link' ], null, 'shortcode' );
+
+        if ( $link === '' ) {
+            return $method( $content );
+        }
+
+        if ( empty( $atts[ 'class' ] ) ) {
+            return sprintf(
+                '<a href="%s">%s</a>',
+                $method( $link ),
+                $method( $content )
+            );
+        }
+
+        return sprintf(
+            '<a href="%s" class="%s">%s</a>',
+            $method( $link ),
+            esc_attr( $atts[ 'class' ] ),
+            $method( $content )
+        );
+    }
+
+    return $method( $content );
 }
 
 // Encode email addresses in a given string
@@ -49,24 +107,10 @@ function devops_ee_encode_emails( $string ) {
 		return $string; // Skip if no @ sign is found
 	}
 
+	 // override encoding function with the 'devops_ee_method' filter
 	$method = apply_filters( 'devops_ee_method', 'devops_ee_encode_str' );
-	$regexp = apply_filters(
-		'devops_ee_regexp',
-		'{
-			(?:mailto:)?
-			(?:
-				[-!#$%&*+/=?^_`.{|}~\w\x80-\xFF]+
-			|
-				".*?"
-			)
-			\@
-			(?:
-				[-a-z0-9\x80-\xFF]+(\.[-a-z0-9\x80-\xFF]+)*\.[a-z]+
-			|
-				\[[\d.a-fA-F:]+\]
-			)
-		}xi'
-	);
+	// override regular expression with the 'devops_ee_regexp' filter
+	$regexp = apply_filters( 'devops_ee_regexp', DEE_REGEXP );
 
 	return preg_replace_callback( $regexp, function ( $matches ) use ( $method ) {
 		return $method( $matches[0] );
@@ -74,23 +118,22 @@ function devops_ee_encode_emails( $string ) {
 }
 
 // Encode string by converting characters to HTML entities
-function devops_ee_encode_str( $string ) {
+function devops_ee_encode_str( $string, $hex = false ) {
+    $chars = str_split( $string );
+    $seed = mt_rand( 0, (int) abs( crc32( $string ) / strlen( $string ) ) );
 
-	$chars = str_split( $string );
-	$seed = mt_rand( 0, (int) abs( crc32( $string ) / strlen( $string ) ) );
+    foreach ( $chars as $key => $char ) {
+        $ord = ord( $char );
 
-	foreach ( $chars as $key => $char ) {
+        if ( $ord < 128 ) { // ignore non-ascii chars
+            $r = ( $seed * ( 1 + $key ) ) % 100; // pseudo "random function"
 
-		$ord = ord( $char );
+            if ( $r > 75 && $char !== '@' && $char !== '.' ); // plain character (not encoded), except @-signs and dots
+            else if ( $hex && $r < 25 ) $chars[ $key ] = '%' . bin2hex( $char ); // hex
+            else if ( $r < 45 ) $chars[ $key ] = '&#x' . dechex( $ord ) . ';'; // hexadecimal
+            else $chars[ $key ] = "&#{$ord};"; // decimal (ascii)
+        }
+    }
 
-		if ( $ord < 128 ) { // Ignore non-ASCII chars
-
-			$r = ( $seed * ( 1 + $key ) ) % 100; // Pseudo-random function
-
-			if ( $r > 60 && $char !== '@' && $char !== '.' ) ; // Keep plain for some chars
-			else if ( $r < 45 ) $chars[ $key ] = '&#x' . dechex( $ord ) . ';'; // Hexadecimal
-			else $chars[ $key ] = '&#' . $ord . ';'; // Decimal
-		}
-	}
-	return implode( '', $chars );
+    return implode( '', $chars );
 }
